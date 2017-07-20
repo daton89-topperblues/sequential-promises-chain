8ef3e0841b206f5bd46b3f375c12f60ba39d81ec
daton <tonydangelo123@gmail.com>
2017-07-20 15:47:58 +0200

    update class

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,68 @@
 /** Class representing an SequentialPromisesChain. */
 export default class SequentialPromisesChain {
+    /**
+     * @param  {boolean} privateforce?
+     */
+    constructor(private force?: boolean) {
+
+    }
 
     /**
-     * Rollback the executed operations if any error occurred.
-     * @param   array - Array of elements passed to the function
-     * @param   function - Function that return a promise.
+     * resolve
+     * @param  {any[]} array - Array of elements passed to the function
+     * @param  {any} predicate - Function that return a promise.
      *                     The function get as parameters:
      *                     element -  array element in order
      *                     index - the index of the current element
-     *                     array - the array
+     * @param  {any} callback?
      * @returns Array of ordered promises - The resolve start when this function is called
      */
-    public resolve(array, fn) {
-        const final = []
-        return array.reduce((promise, element, index, array) => {
-            return promise.then((result) => {
-                return fn(element, index, array).then((res) => {
-                    final.push(res)
-                    return final
-                })
+    public resolve(array: any[], predicate: any, iteratee?: any): any {
+
+        if (iteratee === undefined) {
+            iteratee = (res) => res
+        }
+
+        const final: any[] = []
+        const fail: any[] = []
+
+        return array.reduce((promise: any, element: any, index: number, array: any[]) => {
+
+            return promise.then((i) => {
+
+                if (this.force) {
+
+                    return predicate(element, index)
+                        .then(iteratee)
+                        .then((result) => {
+
+                            final.push(result)
+
+                            return { fail, final }
+                        })
+                        .catch((err) => {
+                            fail.push(err)
+
+                            return { fail, final }
+                        })
+
+                } else {
+
+                    return predicate(element, index)
+                        .then(iteratee)
+                        .then((result) => {
+
+                            final.push(result)
+
+                            return final
+                        })
+
+                }
+
             })
+
         }, Promise.resolve())
+
     }
 
 }
